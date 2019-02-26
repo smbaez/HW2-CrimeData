7f4ab745da24cd1ec162fe15527d1353961eaa1f
smbaez <47392548+smbaez@users.noreply.github.com>
2019-02-25 21:08:44 -0500

    Add files via upload
    finished HW2

--- exercise.js
+++ exercise.js
@@ -1,54 +1,48 @@
 var exercise = {};
 
 exercise.countRecords = function(data){
-    //-------------------------------------------
-    // YOUR CODE
-    // Return the total number of records
-    //-------------------------------------------
-    return 'Error: countRecords not implemented'; 
+    return data.length;
 };
 
 exercise.countDistrictCrimes = function(data,district){
-    //-------------------------------------------
-    // YOUR CODE
-    // Return the number of crimes 
-    // for the given district
-    //-------------------------------------------
-    return 'Error: countDistrictCrimes not implemented'; 
+    counter = 0
+    function countDistrict(entry){
+        if (entry[19]===district){
+            counter = counter +1
+        }
+    }
+    data.forEach(countDistrict)
+    return counter;
 };
 
 exercise.countPrimaryType = function(data,primaryType){
-    //-------------------------------------------
-    // YOUR CODE
-    // Return the number of crimes 
-    // for the giving primary type
-    //-------------------------------------------
-    return 'Error: countPrimaryType not implemented';
+    counter = 0
+    function countType(entry){
+        if (entry[13]===primaryType){
+            counter = counter +1
+        }
+    }
+    data.forEach(countType)
+    return counter;
 };
 
 exercise.countLocation = function(data,location){
-    //-------------------------------------------
-    // YOUR CODE
-    // Return the number of crimes 
-    // for the given location
-    //-------------------------------------------
-    return 'Error: countLocation not implemented';
+    counter = 0
+    function countLocation(entry){
+        if (entry[15]===location){
+            counter = counter + 1
+        }
+    }
+    data.forEach(countLocation)
+    return counter;
 };
 
 
 exercise.buildLatLngPoint = function(crime){
-    //-------------------------------------------
-    // YOUR CODE
-    // build the point for the given crime
-    // 
-    // For example:
-    //
-    // var point = {};    
-    // point.latitude = crime[latitudeColumnNumber];
-    // point.longitude = crime[longitudeColumnNumber];
-    // return point;    
-    //-------------------------------------------    
-    return 'Error: buildLatLngPoint not implemented';
+    var point = {};
+    point.latitude = crime[26];
+    point.longitude = crime[28];
+    return point;  
 };
 
 
